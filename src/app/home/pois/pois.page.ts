70cbb7c6c0bd5c4e9900f84f8685807fd085d183
Audric Dubuis <audric.dubuis@heig-vd.ch>
2019-12-12 15:53:54 +0100

    Adding some routes

---
+++ src/app/home/pois/pois.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pois',
+  templateUrl: './pois.page.html',
+  styleUrls: ['./pois.page.scss'],
+})
+export class PoisPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
