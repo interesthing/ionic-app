a77b62d500e16037207390e06f55d010e94b8bf6
Steve Maibach <steve.maibach@heig-vd.ch>
2019-12-19 12:09:05 +0100

    Show Poi and bug fixed

---
+++ src/app/home/show-poi/show-poi-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ShowPoiPage } from './show-poi.page';
+
+const routes: Routes = [
+  { 
+    path: ':id', 
+    component: ShowPoiPage 
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ShowPoiPageRoutingModule { }
---
+++ src/app/home/show-poi/show-poi.page.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ShowPoiPage } from './show-poi.page';
+
+describe('ShowPoiPage', () => {
+  let component: ShowPoiPage;
+  let fixture: ComponentFixture<ShowPoiPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowPoiPage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ShowPoiPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
